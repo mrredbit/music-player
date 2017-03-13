bee4d98fb4112f77c29243f225ea765333f7bf70
david <whatdoyou@ttth.ink>
2017-03-13 14:55:45 +0000

    Add jsonserver support to the project

--- src/config/base.js
+++ src/config/base.js
@@ -3,7 +3,7 @@
 // Settings configured here will be merged into the final config object.
 export default {
   api: {
-    urlBase: 'http://localhost:5000/api/v1/'
+    urlBase: 'http://localhost:3000/api/v1/'
     // urlBase: 'http://35.185.70.59:3000/api/v1/' // Production Url (leave here for local testing toward production server)
   },
   app: {
